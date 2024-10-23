9a78c39bcf08724471d983df79a1b8324c47cdd3
LukaTree7 <143844079+LukaTree7@users.noreply.github.com>
2024-10-22 20:22:32 -0400

    Add files via upload
    SQL queries to create table with some example instances

--- create_tables.sql
+++ create_tables.sql
@@ -70,3 +70,33 @@ CREATE TABLE AuctionHouse (
     SuccessfulPurchaseID INT,
     FOREIGN KEY (SuccessfulPurchaseID) REFERENCES Purchase(PurchaseID)
 );
+
+INSERT INTO Accounts (AccountID, Username, Password) VALUES
+(1, 'buyer1', 'buyer123'),
+(2, 'seller1', 'seller123'),
+(3, 'buyer2', 'buyer456'),
+(4, 'seller2', 'seller456');
+
+INSERT INTO BuyerAccount (AccountID, AvailableFunds, TotalFunds, IsFrozen) VALUES
+(1, 1000.00, 1500.00, FALSE),
+(3, 2000.00, 3000.00, FALSE);
+
+INSERT INTO SellerAccount (AccountID, Funds, IsFrozen, IsClosed) VALUES
+(2, 500.00, FALSE, FALSE),
+(4, 750.00, FALSE, FALSE);
+
+INSERT INTO Item (ItemID, Name, Description, Images, InitialPrice, StartDate, Duration, IsPublished, IsFrozen, IsArchived, IsComplete, IsFailed, SellerID) VALUES
+(1, 'Antique Vase', 'A rare antique vase from the 18th century.', '[{"url":"image1.jpg"}, {"url":"image2.jpg"}]', 300.00, '2024-10-22 10:00:00', '2024-11-22 10:00:00', TRUE, FALSE, FALSE, FALSE, FALSE, 2),
+(2, 'Vintage Painting', 'An original painting from the 19th century.', '[{"url":"painting1.jpg"}]', 500.00, '2024-10-21 12:00:00', '2024-11-21 12:00:00', TRUE, FALSE, FALSE, FALSE, FALSE, 4);
+
+INSERT INTO Bid (BidID, BuyerID, ItemID, BidAmount) VALUES
+(1, 1, 1, 350.00),
+(2, 3, 2, 600.00);
+
+INSERT INTO Purchase (PurchaseID, ItemID, Name, Description, Images, PurchasePrice, AuctionHouseProfit) VALUES
+(1, 1, 'Antique Vase', 'A rare antique vase from the 18th century.', '[{"url":"image1.jpg"}, {"url":"image2.jpg"}]', 350.00, 35.00),
+(2, 2, 'Vintage Painting', 'An original painting from the 19th century.', '[{"url":"painting1.jpg"}]', 600.00, 60.00);
+
+INSERT INTO AuctionHouse (Funds, SuccessfulPurchaseID) VALUES
+(10000.00, 1),
+(15000.00, 2);

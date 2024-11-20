c6b3a0af120d0ee256d1420799285bb3f2c6fd0c
yang-yilu <101540836+yang-yilu@users.noreply.github.com>
2024-11-19 23:57:33 -0500

    Update createAccount.mjs

--- Lambda/Buyer/createAccount.mjs
+++ Lambda/Buyer/createAccount.mjs
@@ -8,64 +8,75 @@ export const handler = async (event) => {
         database: "auction_house"
     });
 
-    const response = {
-        success: {
-            code: 200,
-            message: "Buyer account created successfully",
-            buyerID: null
-        },
-        error: [
-            {
+    let response = {}
 
-                code: 400,
-                message: "Buyer Account cannot be created"
-            },
-            {
-                
-                code: 401,
-                message: "Username exists"
-            }
-        ]
-    };
+    const dbError = {
+        status: 402,
+        message: "DB Error"
+    }
 
     const createAccount = (username, password) => {
         return new Promise((resolve, reject) => {
             // check if the username already exists
-            pool.query("SELECT * FROM Accounts WHERE Username=?", [username], (error, rows) => {
-                if (error) {
-                    return reject("Database error: " + error);
-                }
+            // pool.query("SELECT * FROM Accounts WHERE Username=?", [username], (error, rows) => {
+            //     if (error) {
+            //         return reject("Database error: " + error);
+            //     }
 
-                if (rows.length > 0) {
-                    //name already exists
-                    response.error[1].message = "Username exists";
-                    return reject(response.error[1]);
-                }
+            //     if (rows.length > 0) {
+            //         //name already exists
+            //         response.error[1].message = "Username exists";
+            //         return reject(response.error[1]);
+            //     }
 
                 // create account
-                pool.query("INSERT INTO Accounts (Username, Password) VALUES (?, ?)", [username, password], (insertError, result) => {
+                pool.query("INSERT INTO Accounts (Username, Password, AccountType) VALUES (?, ?, ?)", [username, password, "Buyer"], (insertError, result) => {
                     if (insertError) {
-                        response.error[0].message = "Buyer Account cannot be created";
-                        return reject(response.error[0]);
+                        response = {
+                            error: dbError
+                        };
+                        reject(insertError);
                     }
 
                     // success created account ,set buyerID
-                    response.success.buyerID = result.insertId; // ID of the created account
-                    return resolve(response.success);
+                    // response.success.buyerID = result.insertId; // ID of the created account
+                    // return resolve(response.success);
+                    let accountID = result.insertId;
+                    console.log(accountID)
+                    pool.query("INSERT INTO BuyerAccount (AccountID, AvailableFunds, TotalFunds) VALUES (?, ?, ?)", [accountID, 0, 0], (insertBuyerError, result) => {
+                        if (insertBuyerError) {
+                            response = {
+                                error: dbError
+                            };
+                            reject(insertBuyerError);
+                        }
+                        if(result){
+                            response = {
+                                status:200,
+                                success: {
+                                    status: 200,
+                                    message: "Buyer Account Created SuccessFully",
+                                    buyerID: accountID
+                                }
+                            }
+                            resolve(result)
+                        }
+                        // console.log(result)
+                    })
                 });
             });
-        });
+        // });
     };
 
     try {
-        const username = event.request.username;
-        const password = event.request.password;
+        const username = event.username;
+        const password = event.password;
 
         // Create account
-        const result = await createAccount(username, password);
-        return { success: result };
+        await createAccount(username, password);
+        return response;
     } catch (error) {
-        return { error: error };
+        return response;
     } finally {
         pool.end();
     }
